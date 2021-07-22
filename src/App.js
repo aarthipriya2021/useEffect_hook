e830c3c20de9f7b8ec495497e5278c5dda7a2752
aarthipriya2021 <a.aarthipriya2021@gmail.com>
2021-07-22 15:10:30 +0530

    useeffect hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, { useState, useEffect } from 'react';
+// export default function Greeting() {
 
-function App() {
+  
+//   const [firstName, setFirstName] = useState(() =>
+//     window.localStorage.getItem('hooksFirstName') || ''
+//   );
+//   const [lastName, setLastName] = useState(() =>
+//     window.localStorage.getItem('hooksLastName') || ''
+//   );
+//   const handleFirstNameChange = (e) => setFirstName(e.target.value);
+//   const handleLastNameChange = (e) => setLastName(e.target.value);
+
+//   useEffect(() => {
+//     window.localStorage.setItem('hooksFirstName', firstName);
+//     window.localStorage.setItem('hooksLastName', lastName);
+//   });
+
+//   return (
+//     <div>
+//       <input value={firstName} onChange={handleFirstNameChange} /><br />
+//       <input value={lastName} onChange={handleLastNameChange} />
+//       <p>
+//         Hello, <span>{firstName} {lastName}</span>
+//       </p>
+//     </div>
+//   );
+// }
+
+
+import React, { useState, useEffect } from 'react';
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
     </div>
   );
 }
-
-export default App;
